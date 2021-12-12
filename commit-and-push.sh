de5274c48f7faded28212352f558b73f479843d5
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-13 09:16:06 +1100

    Added scripts

---
+++ commit-and-push.sh
@@ -0,0 +1,10 @@
+echo "Committing and pushing to repository..."
+echo ""
+
+bash clean.sh && \
+git commit -am "$1" && \
+
+sh push.sh
+
+echo ""
+echo "Finished committing and pushing to repository"
