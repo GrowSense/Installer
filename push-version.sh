f26f09c3e55a7f1992c951c48956082bedfc0736
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-13 10:33:11 +1100

    Updated build number

--- push-version.sh
+++ push-version.sh
@@ -3,6 +3,8 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$BRANCH" = "dev" ];  then
+  git add buildnumber.txt && \
+  git add full-version.txt && \
   git commit -am "Updated build number" && \
   git pull -X theirs origin $BRANCH --quiet && \
   git push origin $BRANCH --quiet
