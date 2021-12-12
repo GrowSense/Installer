3218c6e9e31641be37e072fe6d536056921c31e6
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-13 09:35:23 +1100

    Working on download and install script

--- scripts-download/download-and-install.sh
+++ scripts-download/download-and-install.sh
@@ -3,16 +3,20 @@ echo "Downloading GrowSense installer then installing..."
 
 BRANCH=$1
 
-if [ ! "$BRANCH"]; then
+if [ ! "$BRANCH" ]; then
   BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 fi
 
-if [ ! "$BRANCH"]; then
+if [ ! "$BRANCH" ]; then
   BRANCH=dev
 fi
 
-RELEASE_URL="https://github.com/GrowSense/Installer/releases/download/v$FULL_VERSION-dev/GrowSense-Installer.$FULL_VERSION-$BRANCH.zip
+FULL_VERSION=$(curl -s -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/GrowSense/Installer/$BRANCH/fullversion.txt")
 
-FULL_VERSION=$(curl -s -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/fullversion.txt")
+echo "  Full version: $FULL_VERSION"
 
-$SUDO wget -nv --no-cache -O - https://raw.githubusercontent.com/GrowSense/Installer/$BRANCH/scripts-download/download-and-install.sh
+RELEASE_URL="https://github.com/GrowSense/Installer/releases/download/v$FULL_VERSION-dev/GrowSense-Installer.$FULL_VERSION-$BRANCH.zip"
+
+echo "  Release URL: $RELEASE_URL"
+
+$SUDO wget -nv --no-cache -O - $RELEASE_URL
