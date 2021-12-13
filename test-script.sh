090b33160562410a09cd1fe5ea98b889b7657d53
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-13 14:48:20 +1100

    Updated scripts

--- test-script.sh
+++ test-script.sh
@@ -1,10 +1,17 @@
 echo "Testing download and install script..."
 
 DIR=$PWD
+
 cd scripts-download/
-OUTPUT=$(bash download-and-install.sh)
+
+TMP_DIR=".tmp"
+
+mkdir -p $TMP_DIR
+
+OUTPUT=$(bash download-and-install.sh $)
 [[ "$OUTPUT" == *"Hello World"* ]] || exit 1
-rm tmp -R
+
+rm $TMP_DIR -R
 cd $PWD
 
 echo "Finished testing script"
