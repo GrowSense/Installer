80b289470fc9f5f57aef305e2b740fdc6bb0b81f
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-17 18:19:49 +1100

    Trying to fix TlsException

--- src/GrowSense.Installer/VersionDetector.cs
+++ src/GrowSense.Installer/VersionDetector.cs
@@ -14,7 +14,7 @@ namespace GrowSense.Installer
       Console.WriteLine("  Detecting GrowSense version from GitHub repo...");
       
       var requestHelper = new WebRequestHelper();
-      var version = requestHelper.HttpGet("https://raw.githubusercontent.com/" + Settings.ProjectFamily + "/" + Settings.ProjectName + "/" + Settings.Branch + "/full-version.txt").Trim();
+      var version = requestHelper.HttpGet("http://raw.githubusercontent.com/" + Settings.ProjectFamily + "/" + Settings.ProjectName + "/" + Settings.Branch + "/full-version.txt").Trim();
 
       Console.WriteLine("    Version: " + version);
       return version;
