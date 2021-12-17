6c51696bcdffb0df36d9f47f1187721ee1688696
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-17 18:06:26 +1100

    Trying to fix TlsException

--- src/GrowSense.Installer/WebRequestHelper.cs
+++ src/GrowSense.Installer/WebRequestHelper.cs
@@ -12,17 +12,19 @@ namespace GrowSense.Installer
 
     public string HttpGet(string url)
     {
+      ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+
       var output = "";
-        var request = HttpWebRequest.Create(url);
-        //request.ContentType = "application/json; charset=utf-8";
-        //request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("username:password"));
-        //request.PreAuthenticate = true;
-        var response = request.GetResponse() as HttpWebResponse;
-        using (var responseStream = response.GetResponseStream())
-        {
-            var reader = new StreamReader(responseStream, Encoding.UTF8);
+      var request = HttpWebRequest.Create(url);
+      //request.ContentType = "application/json; charset=utf-8";
+      //request.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("username:password"));
+      //request.PreAuthenticate = true;
+      var response = request.GetResponse() as HttpWebResponse;
+      using (var responseStream = response.GetResponseStream())
+      {
+        var reader = new StreamReader(responseStream, Encoding.UTF8);
         output = reader.ReadToEnd();
-        }
+      }
 
       return output;
     }
