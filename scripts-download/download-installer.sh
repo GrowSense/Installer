0287d925b923554000d4aefb0fbc55ea31fad746
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-18 11:00:01 +1100

    Added argument handling

--- scripts-download/download-installer.sh
+++ scripts-download/download-installer.sh
@@ -1,76 +1,111 @@
 echo ""
 echo "[GSInstaller | download-installer.sh] Downloading GrowSense installer then installing..."
 
-BRANCH=$1
-GROWSENSE_BASE_DIR=$2
 
-if [ ! "$BRANCH" ]; then
-  echo "  [GSInstaller | download-installer.sh] Branch not provided as argument. Getting branch from local git repo..."
-  BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+
+    case "$KEY" in
+            --branch)              branch=${VALUE} ;;
+            --to)    install_to=${VALUE} ;;     
+            *)   
+    esac    
+
+
+done
+
+echo "[GSInstaller | download-installer.sh]   Branch: $branch"
+echo "[GSInstaller | download-installer.sh]   Install to: $install_to"
+
+#BRANCH=$1
+#install_to=$2
+
+#if [ ! "$branch" ]; then
+#  echo "  [GSInstaller | download-installer.sh] Branch not provided as argument. Getting branch from local git repo..."
+#  BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+#fi
+
+if [ ! "$branch" ]; then
+  echo "[GSInstaller | download-installer.sh]   Branch not provided as argument. Using 'dev' default..."
+  branch=dev
 fi
 
-if [ ! "$BRANCH" ]; then
-  echo "  [GSInstaller | download-installer.sh] Branch not provided as argument. Using 'dev' default..."
-  BRANCH=dev
+if [ ! "$install_to" ]; then
+  echo "[GSInstaller | download-installer.sh]   GrowSense base directory not provided as argument. Using '/usr/local/GrowSense/' default..."
+  install_to="/usr/local/"
 fi
 
-if [ ! "$GROWSENSE_BASE_DIR" ]; then
-  echo "  [GSInstaller | download-installer.sh] GrowSense base directory not provided as argument. Using '/usr/local/GrowSense/' default..."
-  BRANCH="/usr/local/GrowSense/"
+
+if [[ "$install_to" == *"Index" ]] || [[ "$install_to" == *"Index/" ]]; then
+  echo "[GSInstaller | download-installer.sh]   Install to contains /Index/. Stripping back to parent directory..."
+  echo "[GSInstaller | download-installer.sh]     Before: $install_to"
+  install_to="$(dirname "$install_to")"
+  echo "[GSInstaller | download-installer.sh]     After: $install_to"
 fi
 
-INSTALLER_DIR="$GROWSENSE_BASE_DIR/Installer"
+if [[ "$install_to" == *"GrowSense" ]] || [[ "$install_to" == *"GrowSense/" ]]; then
+  echo "[GSInstaller | download-installer.sh]   Install to contains /GrowSense/. Stripping back to parent directory..."
+  echo "[GSInstaller | download-installer.sh]     Before: $install_to"
+  install_to="$(dirname "$install_to")"
+  echo "[GSInstaller | download-installer.sh]     After: $install_to"
+fi
 
-echo "[GSInstaller | download-installer.sh]  Branch: $BRANCH"
-echo "[GSInstaller | download-installer.sh]  GrowSense Base Directory: $GROWSENSE_BASE_DIR"
-echo "[GSInstaller | download-installer.sh]  Installer Directory: $INSTALLER_DIR"
+installer_dir="$install_to/GrowSense/Installer"
+growsense_dir="$install_to/GrowSense"
+
+echo "[GSInstaller | download-installer.sh]  Branch: $branch"
+echo "[GSInstaller | download-installer.sh]  GrowSense Base Directory: $growsense_dir"
+echo "[GSInstaller | download-installer.sh]  Installer Directory: $installer_dir"
 
 echo "[GSInstaller | download-installer.sh]  Checking for local installer zip file..."
-echo "[GSInstaller | download-installer.sh]    $INSTALLER_DIR/GrowSenseInstaller.zip"
+echo "[GSInstaller | download-installer.sh]    $installer_dir/GrowSenseInstaller.zip"
 
-if [ -f "$INSTALLER_DIR/GrowSenseInstaller.zip" ]; then
+if [ -f "$installer_dir/GrowSenseInstaller.zip" ]; then
 
   echo "[GSInstaller | download-installer.sh]  Found installer zip on filesystem..."
-  echo "[GSInstaller | download-installer.sh]    $INSTALLER_DIR/GrowSenseInstaller.zip"
+  echo "[GSInstaller | download-installer.sh]    $installer_dir/GrowSenseInstaller.zip"
   
   echo "  Unzipping.."
 
-  unzip -o $INSTALLER_DIR/GrowSenseInstaller.zip -d $INSTALLER_DIR/ || exit 1
+  unzip -o $installer_dir/GrowSenseInstaller.zip -d $installer_dir/ || exit 1
 
 else
 
-  FULL_VERSION_URL="https://raw.githubusercontent.com/GrowSense/Installer/$BRANCH/full-version.txt?R=$(date +%s)"
+  full_version_url="https://raw.githubusercontent.com/GrowSense/Installer/$branch/full-version.txt?R=$(date +%s)"
 
-  echo "[GSInstaller | download-installer.sh]   Full version URL: $FULL_VERSION_URL"
+  echo "[GSInstaller | download-installer.sh]   Full version URL: $full_version_url"
 
-  FULL_VERSION=$(curl -sL "$FULL_VERSION_URL" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" -H "Expires: 0")
-  #FULL_VERSION=$(wget --no-cache -O - "$FULL_VERSION_URL")
+  full_version=$(curl -sL "$full_version_url" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" -H "Expires: 0")
+  #full_version=$(wget --no-cache -O - "$full_version_url")
 
 
-  echo "[GSInstaller | download-installer.sh]   Full version: $FULL_VERSION"
+  echo "[GSInstaller | download-installer.sh]   Full version: $full_version"
 
-  RELEASE_URL="https://github.com/GrowSense/Installer/releases/download/v$FULL_VERSION-dev/GrowSense-Installer.$FULL_VERSION-$BRANCH.zip"
+  release_url="https://github.com/GrowSense/Installer/releases/download/v$full_version-dev/GrowSense-Installer.$full_version-$branch.zip"
 
-  echo "[GSInstaller | download-installer.sh]   Release URL: $RELEASE_URL"
+  echo "[GSInstaller | download-installer.sh]   Release URL: $release_url"
 
-  DOWNLOADED_FILE=$PWD/GrowSenseInstaller.zip
+  downloaded_file=$PWD/GrowSenseInstaller.zip
 
   echo ""
   echo "[GSInstaller | download-installer.sh]  Downloading release file..."
 
-  #curl -L $RELEASE_URL -O $DOWNLOADED_FILE
-  wget $RELEASE_URL -O $DOWNLOADED_FILE || exit 1
+  #curl -L $release_url -O $downloaded_file
+  wget $release_url -O $downloaded_file || exit 1
 
-  mkdir -p $INSTALLER_DIR || exit 1
+  mkdir -p $installer_dir || exit 1
 
   echo ""
   echo "[GSInstaller | download-installer.sh]  Unzipping release file..."
-  unzip -o $DOWNLOADED_FILE -d $INSTALLER_DIR/ || exit 1
+  unzip -o $downloaded_file -d $installer_dir/ || exit 1
 
 fi
 
 echo ""
 echo "[GSInstaller | download-installer.sh]  Launching installer..."
-sudo mono $INSTALLER_DIR/GSInstaller.exe --branch=$BRANCH --install-to=$GROWSENSE_BASE_DIR --allow-skip-download=true || exit 1
+sudo mono $installer_dir/GSInstaller.exe --branch=$branch --install-to=$install_to --allow-skip-download=true || exit 1
 
 echo "[GSInstaller | download-installer.sh] Finished downloading installer."
