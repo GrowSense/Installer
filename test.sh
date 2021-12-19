24b14dc65a82e685d3762c76eb94524917aec669
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-20 10:29:35 +1100

    Added test script for nunit tests

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
- 
+echo "Running NUnit tests..."
+
+DIR=$PWD
+
+#if [ -z "$CATEGORY" ]; then
+#    CATEGORY="Unit"
+#fi
+
+#CATEGORY_INCLUDE=" --include=$CATEGORY"
+
+mono lib/NUnit.Runners/tools/nunit-console.exe bin/Release/*Tests.dll #--include="$CATEGORY"
+
+echo "" && \
+echo "Testing complete"
+
