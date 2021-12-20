eb4adcc7013b16cde6770388fb1d53cc0fd55fef
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-20 12:27:33 +1100

    Fixing tests

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ DIR=$PWD
 
 #CATEGORY_INCLUDE=" --include=$CATEGORY"
 
-mono lib/NUnit.Runners/tools/nunit-console.exe bin/Release/*Tests.dll #--include="$CATEGORY"
+mono lib/NUnit.Runners/tools/nunit-console.exe bin/Release/*Tests.dll || exit 1 #--include="$CATEGORY"
 
 echo "" && \
 echo "Testing complete"
