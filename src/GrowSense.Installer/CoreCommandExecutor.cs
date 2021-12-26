a861a08a9f9616b2877aba6ab2d5162752e0d8e7
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-26 11:56:21 +1100

    Added CoreCommandExecutor

---
+++ src/GrowSense.Installer/CoreCommandExecutor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace GrowSense.Installer
+{
+  public class CoreCommandExecutor
+  {
+    public ProcessStarter Starter;
+    public Settings Settings;
+    public bool IsError
+    {
+      get { return Starter.IsError; }
+    }
+    
+    public CoreCommandExecutor(Settings settings)
+    {
+      Settings = settings;
+      Starter = new ProcessStarter(settings.IndexDirectory);
+    }
+
+    public void ExecuteCoreCommand(string command)
+    {
+      if (!command.StartsWith("bash gs.sh"))
+        command = "bash gs.sh " + command;
+        
+      Starter.Start(command);
+    }
+  }
+}
