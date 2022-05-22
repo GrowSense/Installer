d7f63f8fcf1c7240421c58feabeb663769ccbd17
CompulsiveCoder <compulsivecoder@gmail.com>
2022-05-22 11:12:14 +1000

    [fix] Reenabled local repository

--- push.sh
+++ push.sh
@@ -1,8 +1,7 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-# Disabled because local repository doesn't currently exist
-#git pull local $BRANCH || echo "Failed to pull from local repository"
-#git push local $BRANCH || echo "Failed to push to local repository"
+git pull local $BRANCH || echo "Failed to pull from local repository"
+git push local $BRANCH || echo "Failed to push to local repository"
 
 git pull lan $BRANCH || echo "Failed to pull from lan repository"
 git push lan $BRANCH || echo "Failed to push to lan repository"
