1d2a093895d4502e9e4c5751fd551c032bc530df
CompulsiveCoder <compulsivecoder@gmail.com>
2022-05-22 13:33:38 +1000

    [fix] Writing exception to console

--- src/GrowSense.Installer/Web/FileDownloader.cs
+++ src/GrowSense.Installer/Web/FileDownloader.cs
@@ -2,40 +2,41 @@
 using System.Net;
 namespace GrowSense.Installer.Web
 {
-  public class FileDownloader
-  {
-    public FileDownloader()
+    public class FileDownloader
     {
-    }
-
-    public void Download(string url, string destination)
-    {
-      Console.WriteLine("  Downloading file...");
-      Console.WriteLine("    URL: " + url);
-      Console.WriteLine("    Destination: " + destination);
-
-      try
-      {
-        WebClient webClient = new WebClient();
-        webClient.DownloadFile(url, destination);
-      }
-      catch (WebException wex)
-      {
-        Console.WriteLine("    WebException occurred.");
-
-        if (wex.ToString().IndexOf("Sharing violation") > -1)
-          throw new Exception("IO sharing violation.");
-        
-        if (((HttpWebResponse)wex.Response).StatusCode == HttpStatusCode.NotFound)
+        public FileDownloader()
         {
-          throw new Exception("File not found at: " + url);
         }
-      }
-      catch (Exception ex)
-      {
-        var starter = new ProcessStarter();
-        starter.Start("wget " + url + " -O " + destination);
-      }
+
+        public void Download(string url, string destination)
+        {
+            Console.WriteLine("  Downloading file...");
+            Console.WriteLine("    URL: " + url);
+            Console.WriteLine("    Destination: " + destination);
+
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(url, destination);
+            }
+            catch (WebException wex)
+            {
+                Console.WriteLine("    WebException occurred.");
+                Console.WriteLine(wex.ToString());
+
+                if (wex.ToString().IndexOf("Sharing violation") > -1)
+                    throw new Exception("IO sharing violation.");
+
+                if (((HttpWebResponse)wex.Response).StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new Exception("File not found at: " + url);
+                }
+            }
+            catch (Exception ex)
+            {
+                var starter = new ProcessStarter();
+                starter.Start("wget " + url + " -O " + destination);
+            }
+        }
     }
-  }
 }
