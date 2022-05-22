699ad08a453ad20c71dda141df91ca731c1596c3
CompulsiveCoder <compulsivecoder@gmail.com>
2022-05-22 11:19:04 +1000

    [fix] Added ci-skip to push version git message

--- push-version.sh
+++ push-version.sh
@@ -3,7 +3,7 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$BRANCH" = "dev" ];  then
-  git commit buildnumber.txt full-version.txt -m "Updated version" && \
+  git commit buildnumber.txt full-version.txt -m "Updated version [ci-skip]" && \
   bash push.sh
 else
   echo "Skipping push version. Only pushed for 'dev' branch not '$BRANCH'"
