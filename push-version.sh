d26cf3fac847405062e2587257aab3a9184ca626
CompulsiveCoder <compulsivecoder@gmail.com>
2022-05-22 11:18:09 +1000

    [fix][feat] Finished implementing versioning

--- push-version.sh
+++ push-version.sh
@@ -3,12 +3,8 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$BRANCH" = "dev" ];  then
-  git add buildnumber.txt && \
-  git commit buildnumber.txt -m "Updated build number" && \
-  git push origin $BRANCH && \
-  git add full-version.txt && \
-  git commit full-version.txt -m "Updated full version" && \
-  git push origin $BRANCH
+  git commit buildnumber.txt full-version.txt -m "Updated version" && \
+  bash push.sh
 else
   echo "Skipping push version. Only pushed for 'dev' branch not '$BRANCH'"
 fi
